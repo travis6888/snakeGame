4e840b7d68902e301e388c147bdd04e1c01fb8c4
Travis <travis6888@yahoo.com>
2014-08-04 15:00:47 -0700

    slide one

--- snake/models.py
+++ snake/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Score(models.Model):
+    choices = (("snake", "snake"),
+               ("mind", 'mind'),)
+    player = models.ForeignKey(User)
+    score = models.PositiveSmallIntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    game = models.CharField(choices=choices, default="Snake", max_length=40)
+
+    def __unicode__(self):
+        return "{} {}".format(self.player, self.score)
